14642d6cab9ad17b89c3090d3acb8f901f8b8a1d
Dilip Suthar <9982dilip@gmail.com>
2023-05-16 14:05:07 +0530

    day_16

---
+++ day_16/day_16.js
@@ -0,0 +1,16 @@
+function DoSomething(val){
+    console.log('External')
+    if(val>6){
+        console.log('Nice')
+    }
+    else{
+        console.log('Not Good')
+    }
+    while(val>0){
+        console.log('chalte raho')
+        val--
+    }
+    for(var i=1;i<5;i++){
+        console.log(6*i)
+    }
+}
